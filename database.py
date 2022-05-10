598675a6b077cd7abdd77322802e33cf54d6e81d
Ivaylo Keremidarski <ivaylo.keremidarski@gmail.com>
2022-05-10 12:16:38 +0300

    Added a method to get a recipe by ID.
    Added a method to query a recipe from the db by recipe_id. Reworked the most_matches() method to call the get_recipe_by_id() method with the id of the recipe with the most matches with user's ingredients. The get_recipe_by_id() method then saves the recipe name, ingredients and instructions to the three instance variables which can be used to display in a GUI.

--- database.py
+++ database.py
@@ -54,6 +54,14 @@ class RecipeSuggester:
 
         return user_ingridients
 
+    def get_recipe_by_id(self, id):
+        with self.connection:
+            recipe = self.connection.execute(GET_RECIPE_BY_ID, (id,)).fetchall()
+
+            self.recipe_name = recipe[0][1]
+            self.recipe_ingredients = recipe[0][2]
+            self.recipe_instructions = recipe[0][3]
+
     def most_matches(self):
         all_ingridients = self.get_all_ingredients()
         user_ingridients = self.get_user_ingridients("Turkey mince, Tortilla, Onion")
@@ -72,12 +80,18 @@ class RecipeSuggester:
                 most_matches = matches
                 recipe_id = all_ingridients[recipe_index][0]
 
-        return recipe_id
+        self.get_recipe_by_id(recipe_id)
 
 
 # recipes1 = RecipeSuggester()
+
 # recipes1.create_table()
 # recipes1.add_recipe("Chicken Burrito", "Chicken meat, Tortilla, Cottage cheese, Greens, Onion, Corn", "Cook the chicken meat. Caramelize the onions. Put the chicken and corn in the pan with the onions. Heat the tortilla on a hot pan. Put the greens as a bed. Then the chicken with the corn and onions. Then put cottage cheese on top and any condiments of your choise. Enjoy!")
 # recipes1.add_recipe("Turkey Burrito", "Turkey mince, Tortilla, Cottage cheese, Greens, Onion, Corn", "Cook the turkey mince. Caramelize the onions. Put the turkey mince and corn in the pan with the onions. Heat the tortilla on a hot pan. Put the greens as a bed. Then the turkey mince with the corn and onions. Then put cottage cheese on top and any condiments of your choise. Enjoy!")
 # recipes1.add_recipe("Fusilli Bolognese", "Fusilli, Turkey mince, Tomato sauce, Garlic, Onion", "Boil the fusilli until they are al dente. Save a little of the water they boiled in for the sauce. Caramelize the onion and the garlic in a pan then add the turkey mince. Add Italian spices, salt and pepper. Add the tomato sauce, lower the heat and let it simmer for a few minutes. When the sauce is ready add the fusilli and a little bit of the water you boiled them in. Serve with cheese or by itself. Enjoy!")
-# print(recipes1.most_matches())
+
+# recipes1.most_matches()
+# recipes1.get_random_recipe()
+# print(recipes1.recipe_name)
+# print(recipes1.recipe_ingredients)
+# print(recipes1.recipe_instructions)
